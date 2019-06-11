fc09a23f5f080d3895bee8aad00b28f305c05906
samhithar <samhithaeega@gmail.com>
2019-06-11 14:43:01 -0500

    Updated

--- src/main/java/com/miracle/cog/gateway/controller/CogGatewayController.java
+++ src/main/java/com/miracle/cog/gateway/controller/CogGatewayController.java
@@ -18,41 +18,53 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.miracle.cog.gateway.utility.ServiceInvocation;
+import com.miracle.cognitive.global.bean.Velocity;
 import com.miracle.common.bean.FeatureWithEstimates;
 import com.miracle.common.response.FeatureResponse;
+import com.miracle.database.bean.Release;
+import com.miracle.utility.DataUtility;
 
 @RestController
 public class CogGatewayController {
+
 	@Autowired
 	private ServiceInvocation serviceInvocation;
+	@Autowired
+	private DataUtility dataUtility;
 
 	@GetMapping(value = "/gatewayService")
 	public ResponseEntity<FeatureResponse> buildFeatures(@RequestParam(value = "version") double version)
 			throws ParseException {
 		FeatureResponse response = new FeatureResponse();
 		try {
-
-			CompletableFuture<List<FeatureWithEstimates>> featureEstimates = serviceInvocation.invokeBacklogService();
-			CompletableFuture<Map<String, Double>> storyStateDetails = serviceInvocation.invokeBandwidthService();
+			Release release = dataUtility.loadRelease(version);
+			if (release == null) {
+				response.setObject("Invalid version provided");
+				response.setSuccess(false);
+				return new ResponseEntity<FeatureResponse>(response, HttpStatus.BAD_REQUEST);
+			}
+			CompletableFuture<List<FeatureWithEstimates>> featureEstimates = serviceInvocation
+					.invokeBacklogService(version);
+			CompletableFuture<List<Velocity>> storyStateDetails = serviceInvocation.invokeBandwidthService(version);
 
 			Map<String, Object> dataMap = new HashMap<>();
 			dataMap.put("featureEstimates", featureEstimates.get());
+			System.out.println("Completed backlog service invocation");
 			dataMap.put("storyStateDetails", storyStateDetails.get());
+			System.out.println("Completed bandwidth calculation service invocation");
 
 			RestTemplate restTemplate = new RestTemplate();
-			String releaseServiceUri = "/releaseReport";
+			String releaseServiceUri = "http://releasereport-service:8080/releasereport-service/releaseReport";
 
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON);
 
 			HttpEntity<?> entity = new HttpEntity<Object>(dataMap, headers);
 
-			FeatureResponse releaseReportResponse = restTemplate.postForObject(releaseServiceUri, entity,
-					FeatureResponse.class);
-
-			response.setObject(releaseReportResponse);
-			response.setSuccess(true);
-			return new ResponseEntity<FeatureResponse>(response, HttpStatus.OK);
+			ResponseEntity<FeatureResponse> releaseReportResponse = restTemplate.postForEntity(releaseServiceUri,
+					entity, FeatureResponse.class);
+			System.out.println("Completed release report service invocation");
+			return releaseReportResponse;
 		} catch (Exception exception) {
 			exception.printStackTrace();
 			response.setObject("Failed to build release features");
--- src/main/java/com/miracle/cog/gateway/utility/ServiceInvocation.java
+++ src/main/java/com/miracle/cog/gateway/utility/ServiceInvocation.java
@@ -2,7 +2,6 @@ package com.miracle.cog.gateway.utility;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.miracle.cognitive.global.bean.Velocity;
 import com.miracle.common.bean.FeatureWithEstimates;
 import com.miracle.common.response.FeatureResponse;
 
@@ -17,8 +17,8 @@ import com.miracle.common.response.FeatureResponse;
 public class ServiceInvocation {
 
 	@Async
-	public CompletableFuture<List<FeatureWithEstimates>> invokeBacklogService() throws Exception {
-		String backlogServiceUrl = "";
+	public CompletableFuture<List<FeatureWithEstimates>> invokeBacklogService(double version) throws Exception {
+		String backlogServiceUrl = "http://backlog-service:8080/backlog-service/backlog?version=" + version;
 		URI uri = new URI(backlogServiceUrl);
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<FeatureResponse> result = restTemplate.getForEntity(uri, FeatureResponse.class);
@@ -30,15 +30,15 @@ public class ServiceInvocation {
 	}
 
 	@Async
-	public CompletableFuture<Map<String, Double>> invokeBandwidthService() throws Exception {
-		String bandwidthServiceUrl = "";
+	public CompletableFuture<List<Velocity>> invokeBandwidthService(double version) throws Exception {
+		String bandwidthServiceUrl = "http://bandwidth-service:8080/bandwidth-service/bandwidth?version=" + version;
 		URI uri = new URI(bandwidthServiceUrl);
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<FeatureResponse> result = restTemplate.getForEntity(uri, FeatureResponse.class);
 		if (!result.getBody().isSuccess()) {
 			throw new Exception("Retrieving bandwidth service response failed");
 		}
-		Map<String, Double> storySprintData = (Map<String, Double>) result.getBody().getObject();
-		return CompletableFuture.completedFuture(storySprintData);
+		List<Velocity> velocityList = (List<Velocity>) result.getBody().getObject();
+		return CompletableFuture.completedFuture(velocityList);
 	}
 }
